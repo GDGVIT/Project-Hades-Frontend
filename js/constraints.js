1aa425ed783aa4272f9240b943616f29e8fad346
Debajit Datta <34594080+debajitdatta2k@users.noreply.github.com>
2019-02-19 10:37:45 +0530

    Constraint Update

---
+++ js/constraints.js
@@ -0,0 +1,36 @@
+function checkName(inputText){
+    var exp = /^[A-Za-z ]+$/;
+    if(inputText.value.match(exp)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function checkPhone(inputText){
+    var exp = /^[0-9]{10}$/;
+    if(inputText.value.match(exp)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function checkEmail(inputText){
+    var exp = /^[A-Za-z0-9.]+@[A-Za-z0-9.]+.[A-Za-z0-9]+$/
+    if(inputText.value.match(exp)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function checkGender(inputText){
+    var exp = /^[MF]$/
+    if(inputText.value.match(exp)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
