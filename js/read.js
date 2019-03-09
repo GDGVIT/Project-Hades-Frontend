bd33813df5aa2042534b83d77b2456c645a36742
Debajit Datta <34594080+debajitdatta2k@users.noreply.github.com>
2019-03-09 20:47:16 +0530

    new updates

--- js/read.js
+++ js/read.js
@@ -8,27 +8,107 @@ var queryString = decodeURIComponent(window.location.search);
             })
             document.getElementById("back").innerHTML = '<a href="partman.html?clubName='+club[1]+'&eventName='+queries[1]+'"><i class="arrowbig left"></i></a>'
             $(document).ready(function (){
-                $("#evload").click(function() {
-                    document.getElementById('showd').style.visibility = 'hidden';
-                    $.ajax({
-                        type: "POST",
-                        url: "http://206.189.133.125/api/v1/simple-projection/project-all",
-                        data:input
-                    }).done(function (data) {
-                        if (true) {
-                            var txt="";
-                            data.rs = data.rs.sort(function(a, b){
-                                if(a.name < b.name) { return -1; }
-                                if(a.name > b.name) { return 1; }
-                                return 0;
-                            })
-                            for (var i=0;i<data.rs.length; i++){
-                                var obj = data.rs[i];
-                                txt = txt + '<a href="edit.html?clubName='+club[1]+'&eventName=' + queries[1] + '&name=' + obj.name + '" style="text-decoration: none;"><div class="row"><div class="evcard"><img class="evphoto" src="lm5.jpg"><div class="evdet"><strong class="evhd">' + obj.name + '</strong><br>'+obj.registrationNumber+'</div><div class="rtarrow" style="float: right;"><i class="arrowbig right"></i></div></div></div></a>';
-                            }
-                            document.getElementById("hola").innerHTML= txt;
-                        }
-                    });
+                
+//                $("#evload").click(function() {
+//                    document.getElementById('showd').style.visibility = 'hidden';
+//                    $.ajax({
+//                        type: "POST",
+//                        url: "http://206.189.133.125/api/v1/simple-projection/project-all",
+//                        data:input
+//                    }).done(function (data) {
+//                        if (true) {
+//                            var txt="";
+//                            data.rs = data.rs.sort(function(a, b){
+//                                if(a.name < b.name) { return -1; }
+//                                if(a.name > b.name) { return 1; }
+//                                return 0;
+//                            })
+//                            for (var i=0;i<data.rs.length; i++){
+//                                var obj = data.rs[i];
+//                                txt = txt + '<a href="edit.html?clubName='+club[1]+'&eventName=' + queries[1] + '&name=' + obj.name + '" style="text-decoration: none;"><div class="row"><div class="evcard"><img class="evphoto" src="lm5.jpg"><div class="evdet"><strong class="evhd">' + obj.name + '</strong><br>'+obj.registrationNumber+'</div><div class="rtarrow" style="float: right;"><i class="arrowbig right"></i></div></div></div></a>';
+//                            }
+//                            document.getElementById("hola").innerHTML= txt;
+//                        }
+//                    });
+//                })
+                $("#evload").click(function(e){
+                    e.preventDefault();
+                    funcshow();
                 })
-            }) 
+                function funcshow() {
+                    var format = $('input[name=format]:checked').val();
+                    if(format==="1"){
+                        $.ajax({
+                            type: "POST",
+                            url: "http://206.189.133.125/api/v1/simple-projection/project-all",
+                            data:input
+                        }).done(function (data) {
+                            if (data) {
+                                var txt="";
+                                data.rs = data.rs.sort(function(a, b){
+                                    if(a.name < b.name) { return -1; }
+                                    if(a.name > b.name) { return 1; }
+                                    return 0;
+                                })
+                                for (var i=0;i<data.rs.length; i++){
+                                    var obj = data.rs[i];
+                                    txt = txt + '<a href="edit.html?clubName='+club[1]+'&eventName=' + queries[1] + '&name=' + obj.name + '" style="text-decoration: none;"><div class="row"><div class="evcard"><img class="evphoto" src="lm5.jpg"><div class="evdet"><strong class="evhd">' + obj.name + '</strong><br>'+obj.registrationNumber+'</div><div class="rtarrow" style="float: right;"><i class="arrowbig right"></i></div></div></div></a>';
+                                }
+                                document.getElementById("hola").innerHTML= txt;
+                            }
+                            else{
+                                document.getElementById("hola").innerHTML= "Nothing to Display.";
+                            }
+                        });
+                    }
+                    else if(format==="2"){
+                        $.ajax({
+                            type: "POST",
+                            url: "http://206.189.133.125/api/v1/simple-projection/project-present",
+                            data:input
+                        }).done(function (data) {
+                            if (data) {
+                                var txt="";
+                                data.rs = data.rs.sort(function(a, b){
+                                    if(a.name < b.name) { return -1; }
+                                    if(a.name > b.name) { return 1; }
+                                    return 0;
+                                })
+                                for (var i=0;i<data.rs.length; i++){
+                                    var obj = data.rs[i];
+                                    txt = txt + '<a href="edit.html?clubName='+club[1]+'&eventName=' + queries[1] + '&name=' + obj.name + '" style="text-decoration: none;"><div class="row"><div class="evcard"><img class="evphoto" src="lm5.jpg"><div class="evdet"><strong class="evhd">' + obj.name + '</strong><br>'+obj.registrationNumber+'</div><div class="rtarrow" style="float: right;"><i class="arrowbig right"></i></div></div></div></a>';
+                                }
+                                document.getElementById("hola").innerHTML= txt;
+                            }
+                            else{
+                                document.getElementById("hola").innerHTML= "Nothing to Display.";
+                            }
+                        });
+                    }
+                    else{
+                        $.ajax({
+                            type: "POST",
+                            url: "http://206.189.133.125/api/v1/simple-projection/project-absent",
+                            data:input
+                        }).done(function (data) {
+                            if (data) {
+                                var txt="";
+                                data.rs = data.rs.sort(function(a, b){
+                                    if(a.name < b.name) { return -1; }
+                                    if(a.name > b.name) { return 1; }
+                                    return 0;
+                                })
+                                for (var i=0;i<data.rs.length; i++){
+                                    var obj = data.rs[i];
+                                    txt = txt + '<a href="edit.html?clubName='+club[1]+'&eventName=' + queries[1] + '&name=' + obj.name + '" style="text-decoration: none;"><div class="row"><div class="evcard"><img class="evphoto" src="lm5.jpg"><div class="evdet"><strong class="evhd">' + obj.name + '</strong><br>'+obj.registrationNumber+'</div><div class="rtarrow" style="float: right;"><i class="arrowbig right"></i></div></div></div></a>';
+                                }
+                                document.getElementById("hola").innerHTML= txt;
+                            }
+                            else{
+                                document.getElementById("hola").innerHTML= data.error;
+                            }
+                        });
+                    }
+                }
+            })
         
