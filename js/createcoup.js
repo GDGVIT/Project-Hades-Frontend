e5005e2359c916c545f7c1a0cbf2218baf7b2359
Debajit Datta <34594080+debajitdatta2k@users.noreply.github.com>
2019-03-18 16:44:23 +0530

    coupons added

---
+++ js/createcoup.js
@@ -0,0 +1,62 @@
+var queryString = decodeURIComponent(window.location.search);
+            queryString = queryString.substring(1);
+            total=queryString.split("&");
+            club=total[0].split("=")
+            var queries = total[1].split("=");
+            document.getElementById("back").innerHTML = '<a href="coupon.html?clubName='+club[1]+'&eventName='+queries[1]+'"><i class="arrowbig left"></i></a>'
+            $(document).ready(function (){
+                $("#load").click(function(e) {
+                    e.preventDefault();
+                    chkConstraints();
+                })
+                function chkConstraints() {
+                    var name = document.getElementById("name");
+                    var desc = document.getElementById("desc");
+                    var day = document.getElementById("day");
+                    if (!checkName(name)) {
+                        document.getElementById('message-f1').innerHTML = 'Enter valid name.'
+                        functionAlert();
+                    }
+                    else if (!checkName(desc)) {
+                        document.getElementById('message-f1').innerHTML = 'Enter valid description.'
+                        functionAlert();
+                    }
+                    else if (day.value.length == 0) {
+                        document.getElementById('message-f1').innerHTML = 'Enter valid day.'
+                        functionAlert();
+                    }
+                    else {         
+                        $.ajax({
+                            type: "POST",
+                            url: "http://206.189.133.125/api/v1/coupons/view-schema",
+                            data: JSON.stringify({"event":queries[1]})
+                        }).done(function(res){
+                            coup=res["rs"]
+                            coup.push({
+                                        "name": $('#name').val(),
+                                        "description": $('#desc').val(),
+                                        "day": $('#day').val()
+                                    })
+                            input = JSON.stringify({                   
+                                "event": queries[1],
+                                "coupons": coup
+                            })
+                            var frm = document.getElementById("create").reset();
+                            $.ajax({
+                                type: "POST",
+                                url: "http://206.189.133.125/api/v1/coupons/create-schema",
+                                data:input
+                            }).done(function (data) {
+                                if(data.rs==="created"){
+                                    document.getElementById('message-f1').innerHTML = 'You have successfully created a coupon'
+                                    functionAlert();
+                                }   
+                                else{
+                                    document.getElementById('message-f1').innerHTML = 'Sorry could not create a coupon'
+                                    functionAlert();
+                                }
+                            });
+                        })
+                    }
+                }
+            })
